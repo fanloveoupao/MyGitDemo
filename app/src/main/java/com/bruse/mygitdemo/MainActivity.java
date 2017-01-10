e6bf277de38b430960b7426880bd773ac7d67b4a
fangk <bruse_ios@163.com>
2017-01-10 12:13:03 +0800

    Merge branch 'master' of E:\MyGitDemos with conflicts.

--- app/src/main/java/com/bruse/mygitdemo/MainActivity.java
+++ app/src/main/java/com/bruse/mygitdemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, Main5Activity.class));
+        startActivity(new Intent(MainActivity.this, Main2Activity.class));
     }
 }
